c3d97e8bd4f8793f775b7ea9f6cdae12c7414652
Tanmay Chauhan <tanmaychauhan111@gmail.com>
2022-11-30 02:21:52 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, url_for, request
+import pandas as pd
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
+# from sklearn.externals import joblib
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    df = pd.read_csv("spam.csv", encoding="latin-1")
+    df.drop(df[2:], axis=1, inplace=True)
+    df['label'] = df['class'].map({'ham': 0, 'spam': 1})
+    X = df['message']
+    y = df['label']
+
+    cv = CountVectorizer()
+    X = cv.fit_transform(X)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.20, random_state=42)
+
+    clf = MultinomialNB()
+    clf.fit(X_train, y_train)
+    score = clf.score(X_test, y_test)
+    print(score)
+    if request.method == 'POST':
+        message = request.form['message']
+        data = [message]
+        vect = cv.transform(data).toarray()
+        pred = clf.predict(vect)
+        print(pred)
+    return render_template('result.html', prediction=pred)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
