8ad6bfc6917947554e81308974176d4dc0f5a08f
Tanmay Chauhan <tanmaychauhan111@gmail.com>
2022-11-30 18:54:09 +0530

    minor changes

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
+# from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 # from sklearn.externals import joblib
 
@@ -26,7 +27,7 @@ def predict():
     cv = CountVectorizer()
     X = cv.fit_transform(X)
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.20, random_state=42)
+        X, y, test_size=0.15, random_state=42)
 
     clf = MultinomialNB()
     clf.fit(X_train, y_train)
